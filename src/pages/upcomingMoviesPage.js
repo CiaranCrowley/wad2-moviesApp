66f303fb5588fe409e78033e19946a01b942c27e
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-11-11 10:40:26 +0000

    Change upcoming movies page to use context

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,18 +1,18 @@
 import React, { useContext } from "react";
 import PageTemplate from '../components/templateMovieListPage'
-import {UpcomingMoviesContext} from '../contexts/upcomingMoviesContext'
+import {MoviesContext} from '../contexts/moviesContext'
 import AddToWatchListButton from '../components/buttons/addToWatchList'
 
 const UpcomingMovies = () => {
-  const context = useContext(UpcomingMoviesContext);
-  const movies = context.movies.filter((m) => {  // New
-    return !("favorite" in m);
+  const context = useContext(MoviesContext);
+  const upcoming = context.upcoming.filter((m) => {  // New
+    return !("upcoming" in m);
   });
 
   return (
     <PageTemplate
       title="Upcoming Movies"
-      movies={movies}  /* Changed */
+      movies={upcoming}  /* Changed */
       action={(movie) => {
         return <AddToWatchListButton movie={movie} />;
       }}
@@ -21,39 +21,3 @@ const UpcomingMovies = () => {
 };
 
 export default UpcomingMovies;
-
-
-// import React, { useState, useEffect } from "react";
-// import StubAPI from "../api/stubAPI";
-// import PageTemplate from '../components/templateMovieListPage'
-// import {getUpcomingMovies} from "../api/tmdb-api";
-
-// const UpcomingMovies = () => {
-//   const [movies, setMovies] = useState([]);
-//   useEffect(() => {
-//     getUpcomingMovies().then(movies => {
-//       setMovies(movies);
-//     });
-//   }, []);
-
-//   const addToFavorites = movieId => {
-//     setMovies(movies => {
-//       const index = movies.map(m => m.id).indexOf(movieId);
-//       StubAPI.add(movies[index]);
-//       let newMoviesState = [...movies]
-//       newMoviesState.splice(index, 1);
-//       return newMoviesState;
-//     });
-//   };
-
-//   return (
-//     <PageTemplate
-//       title='Upcoming Movies'
-//       movies={movies}
-//       buttonHandler={addToFavorites}
-//     />
-// );
-
-// };
-// export default UpcomingMovies;
-
